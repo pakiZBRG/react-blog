551c3375ec02436dcbb8f88ddd300a2a12845801
unknown <pavlovicnikola511@gmail.com>
2020-09-11 16:14:46 +0200

    security

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,39 @@
 const express = require('express');
+const User = require('../model/User');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World');
+//Create an account
+router.post('/register', async (req, res) => {
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).json({message: 'Email exists. Try another one'});
+
+    const user = new User(req.body);
+    user.save()
+        .then(user => {
+            res.status(201).json({
+                message: 'User Created',
+                url: `${req.protocol}://${req.get('host')}/api/users/${user.id}`
+            });
+        })
+        .catch(err => res.status(500).json({ 
+            message: "Creation Failed", 
+            error: err 
+        }));
+})
+
+// Get info about user
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            res.status(200).json({
+                _id: user.id,
+                username: user.username,
+                email: user.email,
+                password: user.password,
+                name: user.name,
+                role: user.role,
+            })
+        })
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const usersRoute = require('./routes/users');
+const cookie = require('cookie-parser');
+const config = require('./config/key').mongoURI;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookie());
 
-app.use('/', usersRoute);
+app.use('/api/users', usersRoute);
 
-mongoose.connect('mongodb+srv://nikola:TzAHc9MmfHw7CNp@practice.mz8xz.mongodb.net/Practice?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(config, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("MongoDB Connected..."))
-    .catch(() => console.log("MongoDB Connection Error"))
+    .catch(() => console.log("MongoDB Connection Error"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
+app.listen(PORT);
